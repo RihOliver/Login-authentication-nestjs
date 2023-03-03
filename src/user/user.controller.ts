60f713a6e043cf4810d75a65814fcbe24afd5553
Richard Oliveira <rih.oliver.97@gmail.com>
2023-03-03 16:57:55 -0300

    Current user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 
 @Controller('user') // define a rota base para as rotas deste controller
 export class UserController {
-  constructor(private readonly userService: UserService) {} //injeta o service de usuário no construtor
+  constructor(private readonly userService: UserService) { } //injeta o service de usuário no construtor
 
+  /* @IsPublic() caso eu queria usar novo usuario sem precisar passar o token */
   @Post() // define a rota POST para a criação de usuário
   create(@Body() createUserDto: CreateUserDto) {
     // acima define o método create para receber os dados do usuário através do DTO
